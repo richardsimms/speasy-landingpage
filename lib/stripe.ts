74feb85f615d482edb7bd0b0caa84cb2babdfabe
Richard Simms <simmsdesign@gmail.com>
2025-05-17 20:50:41 +1000

    fix build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,9 @@
-import { Stripe, loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import type { Stripe as StripeClient } from '@stripe/stripe-js';
+import { Stripe } from 'stripe';
 
 // Loading Stripe on the client side
-let stripePromise: Promise<Stripe | null>;
+let stripePromise: Promise<StripeClient | null>;
 export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
@@ -10,5 +12,7 @@ export const getStripe = () => {
 };
 
 // For server-side operations
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-04-30.basil',  // Latest API version
+});
 export { stripe }; 
